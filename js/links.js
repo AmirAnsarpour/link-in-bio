5367fb90d22b578f6df415c6d9765eadc0752d25
AmirAnsarpour <amir.ansarpour2003@gmail.com>
2023-12-08 18:23:29 +0330

    fix

--- js/links.js
+++ js/links.js
@@ -3,4 +3,8 @@ export const links = [
         link: "https://www.instagram.com/amiransarpour_",
         icon: "bi bi-instagram",
     },
+    {
+        link: "https://t.me/AmirAnsarpour",
+        icon: "bi bi-telegram",
+    },
 ]
