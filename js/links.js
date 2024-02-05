7aa5036c7efde0ae00847e9e5bacd6d2e654e6ce
AmirAnsarpour <amir.ansarpour2003@gmail.com>
2024-02-05 16:01:42 +0330

    update links

--- js/links.js
+++ js/links.js
@@ -1,14 +1,26 @@
 export const links = [
-    {
-        link: "https://www.instagram.com/amiransarpour_",
-        icon: "bi bi-instagram",
-    },
-    {
-        link: "https://t.me/AmirAnsarpour",
-        icon: "bi bi-telegram",
-    },
-    {
-        link: "https://discord.gg/b7QcUXAd",
-        icon: "bi bi-discord",
-    },
-]
+  {
+    link: "https://www.instagram.com/amiransarpour_",
+    icon: "bi bi-instagram",
+  },
+  {
+    link: "https://www.linkedin.com/in/amiransarpour/",
+    icon: "bi bi-linkedin",
+  },
+  {
+    link: "mailto:amir.ansarpour2003@gmail.com",
+    icon: "bi bi-envelope-fill",
+  },
+  {
+    link: "https://t.me/AmirAnsarpour",
+    icon: "bi bi-telegram",
+  },
+  {
+    link: "https://discord.gg/b7QcUXAd",
+    icon: "bi bi-discord",
+  },
+  {
+    link: "https://www.youtube.com/@AmirAnsarpour",
+    icon: "bi bi-youtube",
+  },
+];
